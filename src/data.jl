13e24ca78e83d45d532bde2616d49f17cd2ac7d0
Sukera <Seelengrab@users.noreply.github.com>
2024-02-04 00:12:46 +0100

    Add documentation to existing `Possibility`

--- src/data.jl
+++ src/data.jl
@@ -27,6 +27,17 @@ Equivalent to calling `map(f, source)`.
 
 The pre-calculated return type of `Map` is a *best effort* and may be wider than
 necessary.
+
+```julia-repl
+julia> using Supposition
+
+julia> makeeven(x) = (x ÷ 2) * 2
+
+julia> pos = map(makeeven, Data.Integers{Int8}())
+
+julia> all(iseven, example(pos, 10_000))
+true
+```
 """
 struct Map{R, S <: Possibility, F} <: Possibility{R}
     source::S
@@ -43,6 +54,15 @@ produce(m::Map, tc::TestCase) = m.map(produce(m.source, tc))
 A `Possibility` representing values from `source` fulfilling `pred`.
 
 Equivalent to calling `filter(f, source)`.
+
+```julia-repl
+julia> using Supposition
+
+julia> pos = filter(iseven, Data.Integers{Int8}())
+
+julia> all(iseven, example(pos, 10_000))
+true
+```
 """
 struct Satisfying{T, S <: Possibility{T}, P} <: Possibility{T}
     source::S
@@ -71,7 +91,7 @@ end
 """
     Bind(source::Possibility, f)
 
-Binds `map` to `source`, i.e., on `produce(::Bind)` this calls `produce` on
+Binds `map` to `source`, i.e., on `produce(::Bind, ::TestCase)` this calls `produce` on
 `source`, the result of which is passed to `f`, the output of which will be used as input
 to `produce` again.
 
@@ -101,6 +121,20 @@ A `Possibility` representing drawing integers from `[minimum, maximum]`.
 The second constructors draws from the entirety of `T`.
 
 Produced values are of type `T`.
+
+```julia-repl
+julia> using Supposition
+
+julia> is = Data.Integers{Int}()
+
+julia> example(is, 5)
+5-element Vector{Int64}:
+ -5854403925308846160
+  4430062772779972974
+    -9995351034504801
+  2894734754389242339
+ -6640496903289665416
+```
 """
 struct Integers{T<:Integer, U<:Unsigned} <: Possibility{T}
     minimum::T
@@ -136,6 +170,18 @@ A `Possibility` representing drawing vectors with length `l` in `min_size <= l <
 holding elements of type `T`.
 
 `min_size` and `max_size` must be positive numbers, with `min_size <= `max_size`.
+
+```julia-repl
+julia> using Supposition
+
+julia> vs = Data.Vectors(Data.Floats{Float16}(); max_size=5)
+
+julia> example(vs, 3)
+3-element Vector{Vector{Float16}}:
+ [9.64e-5, 9.03e3, 0.04172, -0.0003352]
+ [9.793e-5, -2.893, 62.62, 0.0001961]
+ [-0.007023, NaN, 3.805, 0.1943]
+```
 """
 struct Vectors{T} <: Possibility{Vector{T}}
     elements::Possibility{T}
@@ -193,6 +239,18 @@ A `Possibility` that always produces `value`.
 !!! warning "Mutable Data"
     The source object given to this `Just` is not copied when `produce` is called.
     Be careful with mutable data!
+
+```julia-repl
+julia> using Supposition
+
+julia> three = Data.Just(3)
+
+julia> example(three, 3)
+3-element Vector{Int64}:
+ 3
+ 3
+ 3
+```
 """
 struct Just{T} <: Possibility{T}
     value::T
@@ -231,6 +289,20 @@ A `Possibility` of producing arbitrary `Char` instances.
 !!! warning "Unicode"
     This will `produce` ANY possible `Char` by default, not just valid unicode codepoints!
     To only produce valid unicode codepoints, pass `valid=true` as a keyword argument.
+
+```julia-repl
+julia> using Supposition
+
+julia> chars = Data.Characters()
+
+julia> example(chars, 5)
+5-element Vector{Char}:
+ '⠺': Unicode U+283A (category So: Symbol, other)
+ '𰳍': Unicode U+30CCD (category Lo: Letter, other)
+ '\\U6ec9c': Unicode U+6EC9C (category Cn: Other, not assigned)
+ '\\U1a05c5': Unicode U+1A05C5 (category In: Invalid, too high)
+ '𓂫': Unicode U+130AB (category Lo: Letter, other)
+```
 """
 struct Characters <: Possibility{Char}
     valid::Bool
@@ -253,6 +325,20 @@ end
 
 A `Possibility` of producing arbitrary `Char` instances that are `isascii`.
 More efficient than filtering [`Characters`](@ref).
+
+```julia-repl
+julia> using Supposition
+
+julia> ascii = Data.AsciiCharacters()
+
+julia> example(ascii, 5)
+5-element Vector{Char}:
+ '8': ASCII/Unicode U+0038 (category Nd: Number, decimal digit)
+ 'i': ASCII/Unicode U+0069 (category Ll: Letter, lowercase)
+ 'R': ASCII/Unicode U+0052 (category Lu: Letter, uppercase)
+ '\\f': ASCII/Unicode U+000C (category Cc: Other, control)
+ '>': ASCII/Unicode U+003E (category Sm: Symbol, math)
+```
 """
 struct AsciiCharacters <: Possibility{Char} end
 
@@ -267,6 +353,20 @@ end
     Text(alphabet::Possibility{Char}; min_len=0, max_len=10_000) <: Possibility{String}
 
 A `Possibility` for generating text containing characters of a given alphabet.
+
+```julia-repl
+julia> using Supposition
+
+julia> text = Data.Text(Data.AsciiCharacters(); max_len=15)
+
+julia> example(text, 5)
+5-element Vector{String}:
+ "U\\x127lxf"
+ "hm\\x172SJ-("
+ "h`\\x03\\0\\x01[[il"
+ "\\x0ep4"
+ "9+Hk3 ii\\x1eT"
+```
 """
 struct Text <: Possibility{String}
     vectors::Vectors{Char}
@@ -290,6 +390,19 @@ A `Possibility` for sampling uniformly from `collection`.
 !!! warning "Mutable Data"
     The source objects from the collection given to this `SampledFrom`
     is not copied when `produce` is called. Be careful with mutable data!
+
+```julia-repl
+julia> using Supposition
+
+julia> sampler = Data.SampledFrom([1, 1, 1, 2])
+
+julia> example(sampler, 4)
+4-element Vector{Int64}:
+ 1
+ 1
+ 2
+ 1
+```
 """
 struct SampledFrom{T, C} <: Possibility{T}
     collection::C
@@ -308,6 +421,19 @@ end
     Booleans() <: Possibility{Bool}
 
 A `Possibility` for sampling boolean values.
+
+```julia-repl
+julia> using Supposition
+
+julia> bools = Data.Booleans()
+
+julia> example(bools, 4)
+4-element Vector{Bool}:
+ 0
+ 1
+ 0
+ 1
+```
 """
 struct Booleans <: Possibility{Bool} end
 
@@ -323,6 +449,20 @@ A `Possibility` for sampling floating point values.
 !!! warning "Inf, Nan"
     This possibility will generate *any* valid instance, including positive
     and negative infinities, signaling and quiet NaNs and every possible float.
+
+```julia-repl
+julia> using Supposition
+
+julia> floats = Data.Floats{Float16}()
+
+julia> example(floats, 5)
+5-element Vector{Float16}:
+  -8.3e-6
+   1.459e4
+   3.277
+ NaN
+  -0.0001688
+```
 """
 struct Floats{T <: Base.IEEEFloat} <: Possibility{T} end
 
