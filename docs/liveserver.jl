060ccd145b5e928f3b14d2bb8db97b7b9f33cb43
Sukera <Seelengrab@users.noreply.github.com>
2024-02-19 20:34:03 +0100

    Add example DB to gitignore, fix LiveServer
    This adds the example DB from building the docs to gitignore.
    Also, disentangle watching the newly built docs from building them.
    That never really worked anyway when the build failed.

--- docs/liveserver.jl
+++ docs/liveserver.jl
@@ -5,20 +5,12 @@ const repo_root = dirname(@__DIR__)
 
 # Make sure docs environment is active and instantiated
 import Pkg
-Pkg.activate(@__DIR__)
+Pkg.activate(expanduser("~/.julia/environments/liveserver"))
 Pkg.instantiate()
-
-# Communicate with docs/make.jl that we are running in live mode
-push!(ARGS, "liveserver")
 
 # Run LiveServer.servedocs(...)
 import LiveServer
-LiveServer.servedocs(;
-    # Documentation root where make.jl and src/ are located
-    foldername = joinpath(repo_root, "docs"),
-    # Extra source folder to watch for changes
-    include_dirs = [
-        # Watch the src folder so docstrings can be Revise'd
-        joinpath(repo_root, "src"),
-    ]
+
+LiveServer.serve(;
+    dir = joinpath(repo_root, "docs", "build")
 )
