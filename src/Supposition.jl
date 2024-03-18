7d2fd76b5b85fa66cc7235a80e93d8ac666a788a
Sukera <Seelengrab@users.noreply.github.com>
2024-03-18 09:11:23 +0100

    Add docstring to main module

--- src/Supposition.jl
+++ src/Supposition.jl
@@ -1,3 +1,19 @@
+"""
+    Supposition.jl
+
+Supposition.jl is a fuzzing & property based testing (PBT) framework,
+integrating nicely with `Test.@testset`. Under the hood it's similar
+to the Python fuzzing framework Hypothesis.
+
+The package features:
+
+ * Generation of (almost) arbitrary instances of types
+ * Minimization of failure cases
+ * Testing of statemachines
+ * Deterministic replaying of failures cases
+
+..and a bunch more! Please check out [the documentation](https://seelengrab.github.io/Supposition.jl/stable/) for more information.
+"""
 module Supposition
 
 export assume!, target!, reject, example
