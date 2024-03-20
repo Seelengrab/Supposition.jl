c3769df0f9d19483ffcbd73ecf94b40c360a902e
Sukera <Seelengrab@users.noreply.github.com>
2024-03-20 16:56:23 +0100

    Add additional features to precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,18 +1,40 @@
 using PrecompileTools
 
 @setup_workload begin
-
     works(i::Int8) = i isa Int8
     breaks(i::Int8) = i isa Int
+    targets(i::Int8) = begin
+        target!(i)
+        i isa Int
+    end
+    events(i::Int8) = begin
+        event!(i)
+        i isa Int
+    end
+    assumes(i::Int8) = begin
+        assume!(true)
+        i isa Int
+    end
+    rejects(i::Int8) = begin
+        reject()
+        i isa Int
+    end
     errors(_) = error()
     double(x) = 2x
 
     @compile_workload begin
         intgen = Data.Integers{Int8}()
+        c = Supposition.merge(Supposition.DEFAULT_CONFIG[]; db=false, record=false, max_examples=10)
         redirect_stdout(devnull) do
-            @check db=false record=false verbose=false works(intgen)
-            @check db=false record=false broken=true breaks(intgen)
-            @check db=false record=false broken=true errors(intgen)
+            @with Supposition.DEFAULT_CONFIG => c begin
+                @check verbose=false works(intgen)
+                @check broken=true breaks(intgen)
+                @check broken=true targets(intgen)
+                @check broken=true events(intgen)
+                @check broken=true assumes(intgen)
+                @check rejects(intgen)
+                @check broken=true errors(intgen)
+            end
         end
         for T in (Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64)
             ig = Data.Integers{T}()
